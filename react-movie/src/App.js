d9da7885d33f2e4cd4a0f5f992459cb041ccb3c9
backlo <hjun4512@gmail.com>
2020-02-06 14:23:39 +0900

    feat: planning the movie component 설계

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -3,23 +3,21 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    count : 0
+    isLoading: true,
+    movies: []
   };
 
-  add = () => {
-    this.setState(current => ({ count : current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count : this.state.count - 1 }));
-  };
+  componentDidMount() {
+    setTimeout(() => {
+        this.setState({isLoading: false, book: true});
+    }, 6000);
+  }
 
   render() {
+    const { isLoading } = this.state;
     return (
-      <div className="App">
-        <h1> The number is : {this.state.count}</h1>
-        <button onClick={this.add}> Add </button>
-        <button onClick={this.minus}> Minus </button>
+      <div>
+        {this.state.isLoading ? "Loading..." : "we are ready"}
       </div>
     );
   }
