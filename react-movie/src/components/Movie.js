03dfb60de6ec872ddf9d4c69a1141ab76d53760f
backlo <hjun4512@gmail.com>
2020-02-13 17:02:52 +0900

    style: 코드 리팩토링

--- react-movie/src/components/Movie.js
+++ react-movie/src/components/Movie.js
@@ -5,32 +5,32 @@ import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
     return (
-        <Link to ={{
-            pathname: `/movie/${id}`,
-            state: {
-                year,
-                title,
-                summary,
-                poster,
-                genres
-            }
-        }}>
         <div className="movie">
-            <img src={poster} alt={title} title={title} />
-            <div className="movie__data">
-                <h3 className="movie__title">{title.slice(0,25)}...</h3>
-                <h5 className="movie__year">{year}</h5>
-                <ul className="movie__genres">
-                    {genres.map((genre, index) => (
-                        <li key={index} className="genres__genre">    
-                            {genre}
-                        </li>
-                    ))}
-                </ul>
-                <p className="movie__summary">{summary.slice(0, 180)}...</p>
-            </div>
+            <Link to={{
+                pathname: `/movie/${id}`,
+                state: {
+                    year,
+                    title,
+                    summary,
+                    poster,
+                    genres
+                }
+            }}>
+                <img src={poster} alt={title} title={title} />
+                <div className="movie__data">
+                    <h3 className="movie__title">{title.slice(0, 25)}...</h3>
+                    <h5 className="movie__year">{year}</h5>
+                    <ul className="movie__genres">
+                        {genres.map((genre, index) => (
+                            <li key={index} className="genres__genre">
+                                {genre}
+                            </li>
+                        ))}
+                    </ul>
+                    <p className="movie__summary">{summary.slice(0, 180)}...</p>
+                </div>
+            </Link>
         </div>
-        </Link>
     );
 }
 
